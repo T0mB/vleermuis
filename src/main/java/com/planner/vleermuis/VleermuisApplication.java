b2d3e7c6bd95b63f2f21b2f60b1e85791b85244f
fera zodd <tom@bkfam.nl>
2024-05-17 20:49:24 +0200

    init

---
+++ src/main/java/com/planner/vleermuis/VleermuisApplication.java
@@ -0,0 +1,13 @@
+package com.planner.vleermuis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VleermuisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VleermuisApplication.class, args);
+	}
+
+}
